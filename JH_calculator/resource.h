530b5259a293a07f22ea2b21cefba4b096c52b85
Jaroslaw Herman <jherman@student.agh.edu.pl>
2022-01-15 13:45:10 +0100

    buttons improved

--- JH_calculator/resource.h
+++ JH_calculator/resource.h
@@ -25,12 +25,16 @@
 #define IDC_BUTTON9                     1014
 #define IDC_BUTTON_MINUS                1015
 #define IDC_BUTTON_BIN                  1016
-#define IDC_BUTTON11                    1017
 #define IDC_BUTTON_OCT                  1017
 #define IDC_BUTTON_PLUS4                1018
 #define IDC_BUTTON_MPLY                 1018
 #define IDC_BUTTON10                    1019
 #define IDC_BUTTON0                     1019
+#define IDC_BUTTON_CLR                  1020
+#define IDC_BUTTON13                    1021
+#define IDC_BUTTON_HEX                  1021
+#define IDC_BUTTON14                    1022
+#define IDC_PROGRESS1                   1023
 
 // Next default values for new objects
 // 
@@ -38,7 +42,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        130
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1018
+#define _APS_NEXT_CONTROL_VALUE         1024
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
